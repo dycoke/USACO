7da1f0780cef68fa0bcdcf9129dcaffe72bed79e
dycoke <97375947+dycoke@users.noreply.github.com>
2022-07-11 14:17:36 -0700

    Create bronze2.cpp

---
+++ 2011-2012/January/bronze2.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+  //commented out the files cuz online judge used stdin/out
+	// freopen("stacking.in", "r", stdin);
+	// freopen("stacking.out", "w", stdout);
+	int n, k;
+	cin >> n >> k;
+	vector<int> prefix(n+1);
+	for(int i = 0; i < k; i++) {
+		int a, b; cin >> a >> b;
+		a--; b--;
+		prefix[a]++;
+		prefix[b+1]--;
+	}
+	for(int i = 1; i <= n; i++) {
+		prefix[i] += prefix[i-1];
+	}
+	sort(prefix.begin(), prefix.end());
+	cout << prefix[((n+1)/2)] << '\n';
+}
