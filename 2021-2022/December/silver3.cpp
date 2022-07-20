a9f3849c0ef5e17f852f044a5beadcb6b7841a55
dycoke <97375947+dycoke@users.noreply.github.com>
2022-07-20 15:21:48 -0700

    bru

---
+++ 2021-2022/December/silver3.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int n, m; cin >> n >> m;
+	vector<pair<int, int>> intervals(n);
+	for(int i = 0; i < n; i++) {
+		cin >> intervals[i].first >> intervals[i].second;
+	}
+	vector<ll> start(2*m+1), stop(2*m+1);
+	vector<ll> a(m+1);
+	for(int i = 0; i < n; i++) {
+		a[intervals[i].first]++;
+	}
+	for(int i = 0; i <= m; i++) {
+		for(int j = 0; j <= m; j++) {
+			start[i+j] += a[i] * a[j];
+		}
+	}
+	vector<ll> b(m+1);
+	for(int i = 0; i < n; i++) {
+		b[intervals[i].second]++;
+	}
+	for(int i = 0; i <= m; i++) {
+		for(int j = 0; j <= m; j++) {
+			stop[i+j] += b[i] * b[j];
+		}
+	}
+	ll ans = 0;
+	for(int i = 0; i <= 2*m; i++) {
+		ans += start[i];
+		cout << ans << '\n';
+		ans -= stop[i];
+	}
+}
