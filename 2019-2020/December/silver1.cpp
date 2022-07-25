e7f2a5c8dec737538f8a6ffd351fe91f8dc48d49
dycoke <97375947+dycoke@users.noreply.github.com>
2022-07-24 20:57:12 -0700

    O(1) RUNTIME BABYYYY

--- 2019-2020/December/silver1.cpp
+++ 2019-2020/December/silver1.cpp
@@ -1,1 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	freopen("moobuzz.in", "r", stdin);
+	freopen("moobuzz.out", "w", stdout);
+	int table[8] = {1, 2, 4, 7, 8, 11, 13, 14};
+	int n; cin >> n;
+	n--;
+	cout << 15*(n/8) + table[n%8] << '\n';
+	return 0;
+	
+}
