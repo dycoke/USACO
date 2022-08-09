bd16ab202d288f9a9619a13c384bc8162b7889b7
Andy Chen <totallynotandychen@gmail.com>
2022-08-08 20:41:52 -0700

    did stuff wrong

--- 2016-2017/December/silver1.cpp
+++ 2016-2017/December/silver1.cpp
@@ -1,53 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> adj[200];
-bool visited[200];
-int cur_size;
-void dfs(int node) {
-	visited[node] = 1;
-	cur_size++;
-	for (int &n : adj[node]) {
-		if(!visited[n]) {
-			dfs(n);
-		}
-	}
-}
-
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
-	freopen("moocast.in", "r", stdin);
-	freopen("moocast.out", "w", stdout);
-	int n; cin >> n;
-	vector<pair<pair<int, int>, int>> wtalkies;
+	freopen("haybales.in", "r", stdin);
+	freopen("haybales.out", "w", stdout);
+	int n, q; cin >> n >> q;
+	vector<int> haybales(n);
 	for(int i = 0; i < n; i++) {
-		int x, y, p;
-		cin >> x >> y >> p;
-		x--; y--;
-		wtalkies.push_back({{x, y}, p});
+		cin >> haybales[i];
 	}
-	for(int i = 0; i < n - 1; i++) {
-		for(int j = i + 1; j < n; j++) {
-			int x = wtalkies[i].first.first - wtalkies[j].first.first;
-			int y = wtalkies[i].first.second - wtalkies[j].first.second;
-			if(x*x + y*y <= wtalkies[i].second*wtalkies[i].second) {
-				adj[i].push_back(j);
-			}
-			if(x*x + y*y <= wtalkies[j].second*wtalkies[j].second) {
-				adj[j].push_back(i);
-			}
+	sort(haybales.begin(), haybales.end());
+	for(int i = 0; i < q; i++) {
+		int a, b; cin >> a >> b;
+		int l = lower_bound(haybales.begin(), haybales.end(), a) - haybales.begin();
+		int r = upper_bound(haybales.begin(), haybales.end(), b) - haybales.begin();
+		if(l == n) {
+			cout << 0 << '\n';
+		} else if(r == n) {
+			cout << n - l << '\n';
+		} else {
+			cout << r - l << '\n';
 		}
 	}
-	int ans = 0;
-	for(int i = 0; i < n; i++) {
-		if(!visited[i]) {
-			memset(visited, 0, sizeof(visited));
-			cur_size = 0;
-			dfs(i);
-			ans = max(ans, cur_size);
-		}
-	}
-	cout << ans << '\n';
 	return 0;
 }
